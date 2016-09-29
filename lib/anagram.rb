1d1489d52f12dca37255679b56ad6fe49ca8b670
Craig Iturbe <craigiturbe@Craigs-MacBook-Air.local>
2016-09-29 14:49:28 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+class Anagram
+
+attr_accessor :word
+
+def initialize (word)
+	@word = word
+end
+
+def alphabetical_word
+	word.split(//).sort!
+end
+
+def match(anagram_array)
+	anagram_array.select do |scrambled_word|
+		scrambled_word.split(//).sort! == alphabetical_word
+	end
+end
+
+end
